3e9442cf749ccb74ac542325c13b0b8155286fd7
rbuit <rbuitendijk94@gmail.com>
2017-06-16 11:14:55 +0200

    damn daniel

--- Serialread/InputController.cs
+++ Serialread/InputController.cs
@@ -47,7 +47,7 @@ namespace serialread
                 {   
                     message = serial.ReadLine();
                     IList<string> serialsplit = message.Split(',').ToList<string>();
-                    
+                    // testen of het splitten goed is gegaan
                     dictionary["setAngle"] = Convert.ToInt32(serialsplit[3]);
                     dictionary["setSpeed"] = Convert.ToInt32(serialsplit[5]);
                     dictionary["buttons"] = Convert.ToInt32(serialsplit[6]);
@@ -58,12 +58,12 @@ namespace serialread
                     IList<String> joystickZsplitter = joystickControlZAndSum.Split('*').ToList<String>();
                     double joystickZ = Convert.ToDouble(joystickZsplitter[0]);
 
+                   
                     if (joystickZ == 1)
                     {
-                        dictionary["joystickX"] = map(joystickX, -800, 1680, -1, 1);
-                        dictionary["joystickY"] = map(joystickY, 880, 1930, -1, 1);
-                        dictionary["joystickZ"] = map(joystickZ,)
-                        dictionary["joystickControlZ"] = Convert.ToDouble(joystickZsplitter[0]);
+                        dictionary["joystickX"] = map(joystickX, -950, 1100, -1, 1);
+                        dictionary["joystickY"] = map(joystickY, -850, 900, -1, 1);
+                        dictionary["joystickZ"] = map(joystickZ, 10, 10, -1, 1);// nog ffkies uitzoeke
                     }
                     //websocket.ControlBoat(dictionary["joystickX"], dictionary[" joystickX"], dictionary[" joystickY"]);
                     
@@ -77,50 +77,6 @@ namespace serialread
             return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
         }
        
-
-
-        /*
-        public void Knoppen()
-        {
-            Dictionary<string, int> dictionary = new Dictionary<string, int>();
-            dictionary.Add("dim",-2 ) ;
-            dictionary.Add("geenKnop",-1 );
-            dictionary.Add("ready", 0);
-            dictionary.Add("alarm", 1);
-            dictionary.Add("ecdis", 2);
-            dictionary.Add("deskControl", 3);
-            dictionary.Add("remoteControl", 4);
-            dictionary.Add("surgeJoystick", 5);
-            dictionary.Add("surgeAuto", 6);
-            dictionary.Add("position", 7);
-            dictionary.Add("swayJoystick", 8);
-            dictionary.Add("swayAuto", 9);
-            dictionary.Add("headingJoystick", 10);
-            dictionary.Add("", 11);
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-            dictionary.Add("", );
-
-        }
-        */
-
-
     }
 
     }
